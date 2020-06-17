fbc4a765e2ffc3d25b948de9ebd3516230593aeb
AlohaOttawa <61981451+AlohaOttawa@users.noreply.github.com>
2020-06-17 10:13:48 -0400

    84 - Associate the billing profile to the order using the Car View

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 import math
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+from billing.models import BillingProfile
 
 from carts.models import Cart
 from DJ2.utils import unique_order_id_generator
@@ -16,6 +17,7 @@ ORDER_STATUS_CHOICES = (
 
 
 class Order(models.Model):
+    billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True, on_delete=models.SET_NULL)
     order_id        = models.CharField(max_length=50, blank=True)
     # billing profile
     # shipping address
@@ -24,6 +26,7 @@ class Order(models.Model):
     status          = models.CharField(max_length=50, default="created", choices=ORDER_STATUS_CHOICES)
     shipping_total  = models.DecimalField(default=5.99, max_digits=10, decimal_places=2)
     total           = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
+    active          = models.BooleanField(default=True)
 
     def __str__(self):
         return self.order_id
